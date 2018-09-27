7f28573eb23b686710d87addc2201cd1629f270e
kawiman <43651943+kawiman@users.noreply.github.com>
2018-09-27 16:05:16 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default{
   file {'/root/README':
-      ensure => file,
-      content => 'Thisis a readme',
+    ensure  => file,
+    content => 'Thisis a readme',
+    owner   => 'root',
   }
 }
